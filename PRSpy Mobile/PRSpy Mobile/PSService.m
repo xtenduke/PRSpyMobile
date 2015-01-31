cdb069723242aab66dc5bd35e5ecd53befeb11aa
Jake Laurie <jake.gordon.laurie@gmail.com>
2015-01-31 14:58:24 +1300

    Added printing server names

--- PRSpy Mobile/PRSpy Mobile/PSService.m
+++ PRSpy Mobile/PRSpy Mobile/PSService.m
@@ -7,6 +7,7 @@
 //
 
 #import "Config.h"
+#import "PSServer.h"
 #import "PSService.h"
 #import "AppDelegate.h"
 #import "AFHTTPRequestOperation.h"
@@ -32,17 +33,28 @@
     NSURL *reqUrl = [NSURL URLWithString:MAINTENANCE_SERVER_URL];
     NSURLRequest *request = [NSURLRequest requestWithURL:reqUrl];
     
+    [NSNumber numberWithInt:1];
     AFHTTPRequestOperation *afRequest = [[AFHTTPRequestOperation alloc] initWithRequest:request];
     afRequest.responseSerializer = [AFJSONResponseSerializer serializer];
     [afRequest setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *afRequest, id responseObject) {
         
         //send back all the 'shopItems'
-        NSArray *serversArray = [responseObject objectForKey:@"Data"];
+        NSArray *serversRawArray = [responseObject objectForKey:@"Data"];
         NSLog(@"DL COMPLETE");
-        for(id servers in serversArray) {
-            NSLog(@"MAP %@",[servers objectForKey:@"MapName"]);
+        
+        NSMutableArray *serversObjArray = [[NSMutableArray alloc]init];
+
+        for(id server in serversRawArray) {
+            PSServer *currentServer = [[PSServer alloc]initWithDict:server];
+            [serversObjArray addObject:currentServer];
             
+            
+            
+            NSLog(@" %@",currentServer.serverName);
         }
+        
+        
+        
         
     } failure:^(AFHTTPRequestOperation *op, NSError *error) {
         NSLog(@"MAIN MAINTENANCE REQUEST ERROR %@",error);
